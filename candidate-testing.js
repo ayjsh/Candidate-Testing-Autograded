65c2da4cc2dd5136c2cbd8f2fa3d5f80880a0776
Ayjsh <Ayjsh@Ayjshs-MBP.attlocal.net>
2024-01-08 15:17:55 -0600

    Part 1

--- candidate-testing.js
+++ candidate-testing.js
@@ -4,10 +4,12 @@ const input = require('readline-sync');
 
 // TODO 1.1a: Define candidateName // 
 let candidateName;
+candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
 let candidateAnswer;
+candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
@@ -18,12 +20,12 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+ candidateName = input.question("What is your name?")
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+candidateAnswer = input.question(question)
 
 }
 
@@ -42,7 +44,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log();
+   console.log("Hello " + candidateName);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
