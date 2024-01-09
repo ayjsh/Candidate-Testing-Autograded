c3817e85113ed50a2aa68e18c34ab37b6b89aa42
Ayjsh <Ayjsh@Ayjshs-MBP.attlocal.net>
2024-01-09 16:06:02 -0600

    take 2

--- candidate-testing.js
+++ candidate-testing.js
@@ -13,27 +13,31 @@ candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions
+let correctAnswers
+let candidateAnswers
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
- candidateName = input.question("What is your name?")
+ candidateName = input.question("What is your name? ")
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question(question)
+  candidateAnswer = input.question(question)
 
 }
 
-function gradeQuiz(candidateAnswers) {
+function gradeQuiz(candidateAnswer) {
 
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
-
+  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly //
+  
+  if (candidateAnswer === correctAnswer) {
+    console.log("You answered Correctly!!!")
+  } else {
+    console.log("Sorry, you answered incorrectly.")
+  }
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
